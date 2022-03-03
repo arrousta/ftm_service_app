7eedf9e2182eecdcf01ae2d2fd9c781903d94eac
Ali <Ali3nti@gmail.com>
2022-03-03 16:28:00 +0330

    Splash Screen doesn't ok and go next step

--- lib/screens/spash_screen.dart
+++ lib/screens/spash_screen.dart
@@ -7,14 +7,14 @@ import 'package:ftm_service_app/constractor.dart';
 import 'package:ftm_service_app/services/network_adapter.dart';
 import 'package:ftm_service_app/structures/user.dart';
 
-class MyHttpOverrides extends HttpOverrides{
+class MyHttpOverrides extends HttpOverrides {
   @override
-  HttpClient createHttpClient(SecurityContext? context){
+  HttpClient createHttpClient(SecurityContext? context) {
     return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
-
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -22,29 +22,51 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   late Future<User> futureUser;
 
-  // FutureBuilder<User> futureBuilderGet() {
+  // FutureBuilder<User> futureBuilderGet(BuildContext context) {
   //   return FutureBuilder<User>(
   //     future: futureUser,
   //     builder: (context, snapshot) {
   //       if (snapshot.hasData) {
-  //         return Text(snapshot.data!.operator.toString());
+  //         return Navigator(
+  //           onGenerateRoute: (settings) {
+  //             setState(()  {
+  //               Duration duration = const Duration(seconds: 100);
+  //               Future.delayed(duration, ()
+  //               {
+  //                 Navigator.pushReplacement(context, MaterialPageRoute(
+  //                   builder: (context) {
+  //                     return HomePage(
+  //                       pageTitle: snapshot.data!.operator.toString(),
+  //                     );
+  //                   },
+  //                 ),);
+  //               });
+  //             });
+  //           },
+  //         );
   //       } else if (snapshot.hasError) {
-  //         return Text('errrror: ${snapshot.error}');
+  //         return Column(
+  //           children: [
+  //             Text('errrror: ${snapshot.error}'),
+  //             // buildButtonReload(),
+  //           ],
+  //         );
   //       }
   //       // By default, show a loading spinner.
-  //       // return const CircularProgressIndicator();
+  //       return const SpinKitCircle(
+  //         color: Colors.indigo,
+  //         size: 50.0,
+  //       );
   //     },
   //   );
   // }
 
   String futureGet() {
-
     futureUser = fetchUser();
     futureUser.then((value) {
-      if(value.operator != null){
+      if (value.operator != null) {
         print(value.operator);
         return value.operator;
       }
@@ -56,28 +78,26 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     HttpOverrides.global = MyHttpOverrides();
+    futureUser = fetchUser();
     getInternetStatus();
   }
 
-
   void getInternetStatus() async {
     String operator = futureGet();
-
     Duration duration = const Duration(seconds: 3);
-    await Future.delayed(duration,() {
+    await Future.delayed(duration, () {
       Navigator.pushAndRemoveUntil(
         context,
         MaterialPageRoute(
           builder: (context) {
             return HomePage(
-              pageTitle: operator,
+              pageTitle: "operator",
             );
           },
         ),
-          (route) => false,
+        (route) => false,
       );
     });
-
   }
 
   @override
@@ -93,9 +113,15 @@ class _SplashScreenState extends State<SplashScreen> {
             color: Colors.indigo,
             size: 50.0,
           ),
+          // (futureUser == null) ? BuildButtonReload() : futureBuilderGet(context),
+
         ],
       )),
       backgroundColor: bgColor2,
     );
+  }
+
+  TextButton BuildButtonReload() {
+    return TextButton(onPressed: () {}, child: const Text("Reload"));
   }
 }
